f61e7cd45a79dd2dc1a2f517d5c97ad89cfeffcb
estrelanfontes <138054925+estrelanfontes@users.noreply.github.com>
2023-07-16 21:22:45 -0300

    Create ex6.py

---
+++ condições aninhadas/ex6.py
@@ -0,0 +1,11 @@
+i=int(input("Qual a sua idade? "))
+if i<=9:
+  print("mirim")
+elif i<=14:
+  print("infantil")
+elif i<=19:
+  print("junior")
+elif i<=25:
+  print("senior")
+else:
+  print("master")
