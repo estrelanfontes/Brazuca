3f8dcee8ea347008275a199161885aa7528246f2
estrelanfontes <138054925+estrelanfontes@users.noreply.github.com>
2023-07-21 04:28:01 -0300

    Update ex4.py

--- aula while/ex4.py
+++ aula while/ex4.py
@@ -1,12 +1,18 @@
+def termos():
+  termos=int(input(f"Quantos termos a mais? "))
+  while termos!=0:
+    termos=int(input(f"Quantos termos a mais? "))
+    if termos==0:
+     break
+
 v=2
 f=0
-q=21
+q=39
 print(f"Primeiro termo: {f}")
 print("Os primeiros 20 numeros: ", end="")
 for p in range(f,q,v):
- print(p,end=", ")
+    print(p,end=", ")
 print("")
 print(f"A razão: {v}")
-def termos():
- int(input(f"Quantos termos a mais? "))
 termos()
+#pequeno detalhe: eu só pergunto quantos termos ele quer ver a mais, mas eu não mostro os termos
