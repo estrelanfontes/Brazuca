46f7a8e5c413536b46f99d94103049afe3ac471e
estrelanfontes <138054925+estrelanfontes@users.noreply.github.com>
2023-07-27 01:53:02 -0300

    Update ex3.py

--- aula dados-dict/ex3.py
+++ aula dados-dict/ex3.py
@@ -1,2 +1,11 @@
-nome = input("nome: ")
-partidas = int(input("quantas partidas: "))
+dados = {"n": input("Qual o seu nome: "), "p": int(input("Quantas partidas ja jogou? "))}
+a=0
+lista = []
+while dados["p"]!=0:
+  gp = int(input("Quantos gols? "))
+  a+=1
+  lista.append(gp)
+  if dados["p"]==a:
+    break
+print(lista)
+#Falta colocar a lista no dicionário
