7beba1661f1608c21e0ecda75aefc2d9de38bee5
estrelanfontes <138054925+estrelanfontes@users.noreply.github.com>
2023-07-29 02:03:13 -0300

    Update ex5.py

--- aula args/ex5.py
+++ aula args/ex5.py
@@ -1,1 +1,9 @@
-
+#Crie uma função que receba um numero inteiro como entrada e retorne uma lista de todos os divisores desse numero
+def f():
+  n = int(input("Digite um numero inteiro: "))
+  lista = []
+  for i in range(1,9):
+    if n%i == 0:
+      lista.append(i)
+  print(f"Os divisores desse numero são {lista}")
+f()
