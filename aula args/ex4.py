cb3dfebd628b85b7c2fa084f1a6b4af2b1e1639c
estrelanfontes <138054925+estrelanfontes@users.noreply.github.com>
2023-07-28 21:29:14 -0300

    Update ex4.py

--- aula args/ex4.py
+++ aula args/ex4.py
@@ -1,2 +1,4 @@
-t = input("Digite o texto a ser invertido:   ")
-print(t[::-1])#Não sei oqq eu fiz
+t = input("Digite um texto, se for um palindromo rebeberá (True) caso contrario (False) :   ")
+r = t[::-1]#Não sei oqq eu fiz
+p = t==r
+print(p)
